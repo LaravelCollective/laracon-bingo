c8d4d66810fea57e409fc02f6615cd0c9777b5c1
Adam Engebretson <adam.engebretson@bridgeconx.com>
2016-07-27 18:44:04 -0400

    Fixing buttons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,11 @@
         <h2>It's Laracon Bingo!</h2>
 
         <div>
-            <a href="/auth/redirect?provider=github" type="button" class="btn btn-secondary">
+            <a href="/auth/redirect?provider=github" class="btn btn-secondary">
                 <i class="fa fa-github"></i>
                 Login with Github
             </a>
-            <a href="/auth/redirect?provider=facebook" type="button" class="btn btn-primary">
+            <a href="/auth/redirect?provider=facebook" class="btn btn-primary">
                 <i class="fa fa-facebook"></i>
                 Login with Facebook
             </a>
