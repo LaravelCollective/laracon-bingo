876bba934a64d44bc33d8ead03f772bcbe652899
Adam Engebretson <adam.engebretson@bridgeconx.com>
2016-07-27 20:48:43 -0400

    Fixing tests

--- tests/integration/TermsTest.php
+++ tests/integration/TermsTest.php
@@ -7,18 +7,38 @@ class TermsTest extends TestCase
 
     use DatabaseMigrations;
 
-    public function test_it_can_fetch_terms_from_the_api()
+    public function test_it_can_fetch_user_terms_from_the_api()
     {
-        factory(\App\Term::class, 5)->create();
+        factory(\App\Term::class, 50)->create();
+        $user = factory(\App\User::class)->make();
 
-        $this->json('GET', '/api/terms')
+        $this->actingAs($user);
+
+        $this->json('GET', '/api/me')
           ->assertResponseOk()
           ->seeJsonStructure([
-            '*' => [
-              'id',
-              'term',
-            ],
+            'name',
+            'email',
+            'terms',
           ]);
+    }
+
+    public function test_it_generates_the_same_list_of_terms_each_time()
+    {
+        factory(\App\Term::class, 50)->create();
+        $user = factory(\App\User::class)->make();
+
+        $this->assertEquals($user->terms, $user->terms);
+    }
+
+    public function test_it_generates_a_unique_list_of_terms_for_different_users()
+    {
+        factory(\App\Term::class, 100)->create();
+        $user1 = factory(\App\User::class)->make();
+        $user2 = factory(\App\User::class)->make();
+
+        $this->assertTrue($user1->terms == $user2->terms);
+
     }
 
 }
