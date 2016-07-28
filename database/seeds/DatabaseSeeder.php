96d79891914275d18d3b45b332b66a7b391cd5d3
Matt Floyd <mattdfloyd@gmail.com>
2016-07-28 15:08:02 -0400

    Seed terms table with random words

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\Term::class, 500)->create()->each(function($term) {
+            $term->save();
+        });
     }
 }
